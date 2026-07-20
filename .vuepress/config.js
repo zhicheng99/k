8d0940a781261ad767c050220c55077449ffbf42
zhicheng <289530302@qq.com>
2026-07-21 01:27:09 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
       //apiKey: "bb89a7555541af96d500213fb23ea66d",
       apiKey:"72e05530eb3e8b95d9a65cef01f2b00c",
       //apiKey:"c64d99d7233c868e667414b42aa4aa54",
-      indexName: "kk",
+      indexName: "book",
     },
     sidebarDepth: 0,
     sidebar: [
