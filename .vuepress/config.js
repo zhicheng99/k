0a89b8b715a6e69d11264e62336aa235538e9252
zhicheng <289530302@qq.com>
2026-07-20 22:39:39 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -27,7 +27,7 @@ module.exports = {
       "children": [
         ["js/js实现类的继承.md", "js实现类的继承"],
         ["js/js拖动效果1.md", "js拖动效果1"],
-        ["js/js拖动效果2.md", "报动效果2"],
+        ["js/js拖动效果2.md", "js拖动效果2"],
         ["js/promise串、并行实现.md", "promise串、并行实现"],
         ["js/promise暂停 继续.md", "promise暂停 继续"],
         ["js/原型方法卸载dom事件（once）.md", "原型方法卸载dom事件"],
